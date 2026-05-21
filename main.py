eedb98c0fdf5acdaa0464c16ab279e95341d47cd
nihilne <85189523+nihilne@users.noreply.github.com>
2026-05-21 22:40:21 +0300

    feat: basic sender class + sample + cli util

---
+++ main.py
@@ -0,0 +1,29 @@
+import sys
+import argparse
+import logging
+from sender import Sender, Speed
+
+root = logging.getLogger()
+root.setLevel(logging.INFO)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.INFO)
+formatter = logging.Formatter(
+    "[%(asctime)s] [%(levelname)s] %(name)s - %(message)s",
+    "%d-%m-%Y %H:%M:%S",
+)
+handler.setFormatter(formatter)
+root.addHandler(handler)
+
+parser = argparse.ArgumentParser(description="File36")
+parser.add_argument("mode", help="Whether to send or receive")
+args = parser.parse_args()
+
+match args.mode:
+    case "send":
+        sender = Sender("./sample", volume=0.8, speed=Speed.ULTRAFAST)
+        sender.play()
+    case "receive":
+        print("Receive")
+    case _:
+        print("Nothing")
--- sender.py
+++ sender.py
@@ -1,2 +1,102 @@
-file = input("File to transfer: ")
-with open()
+import logging
+import numpy as np
+import sounddevice as sd
+from pathlib import Path
+from enum import Enum
+from typing import Annotated
+
+
+class Speed(Enum):
+    SLOW = 1000
+    MEDIUM = 500
+    FAST = 300
+    SUPERFAST = 150
+    ULTRAFAST = 50
+    HYPERFAST = 10
+
+
+class Sender:
+    SAMPLE_RATE = 44100
+    FREQ_ZERO = 1200
+    FREQ_ONE = 2200
+    FREQ_SYNC = 1700
+
+    def __init__(
+        self,
+        file_path: str,
+        speed: Speed = Speed.MEDIUM,
+        volume: Annotated[float, "0.0 to 1.0"] = 0.1,
+    ):
+        self.file_path = Path(file_path)
+        self.speed = speed
+        self.volume = volume
+
+    def read_bytes(self):
+        """Reads raw bytes of the file speficied."""
+        with self.file_path.open("rb") as file:
+            return file.read()
+
+    def to_bits(self, data: bytes):
+        bits = []
+        for byte in data:
+            for i in range(7, -1, -1):  # MSB for some reason
+                bits.append((byte >> i) & 1)
+        return bits
+
+    def tone(self, freq: int, duration: int):
+        """Generates a tone given a frequency and a duration in milliseconds."""
+        d = duration / 1000.0
+        t = np.linspace(0, d, int(self.SAMPLE_RATE * d), False)
+        sine = np.sin(2 * np.pi * freq * t)
+        return sine.astype(np.float32) * self.volume
+
+    def sequence(self, sequence: list[tuple[int, int]]):
+        """
+        Generates a sequence of tones given a list of
+        tones containing the frequency and duration
+        (in ms) of each tone.
+        """
+        parts = []
+        for freq, duration in sequence:
+            parts.append(self.tone(freq, duration))
+
+        return np.concatenate(parts)
+
+    def header(self):
+        tones = [
+            (600, 50),
+            (0, 50),
+            (600, 50),
+            (700, 50),
+            (1900, 300),
+            (1200, 50),
+            (1900, 300),
+        ]
+        return self.sequence(tones)
+
+    def encode(self):
+        data = self.read_bytes()
+        bits = self.to_bits(data)
+        sequence = []
+        for bit in bits:
+            freq = self.FREQ_ONE if bit == 1 else self.FREQ_ZERO
+            sequence.append((freq, self.speed.value))
+            sequence.append((self.FREQ_SYNC, self.speed.value))
+
+        return self.sequence(sequence)
+
+    def play(self):
+        all = np.concatenate(
+            [
+                self.header(),
+                self.tone(self.FREQ_SYNC, self.speed.value * 2),
+                self.encode(),
+            ]
+        )
+        sd.play(all, self.SAMPLE_RATE)
+        bps = 1000 / self.speed.value
+        logging.info(
+            f"Playing header + data ({len(all) / self.SAMPLE_RATE}s total @ {bps}bits/s or {bps / 8}bytes/s)"
+        )
+        sd.wait()
+        logging.info("Done!")
