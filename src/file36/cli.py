4f2ed52831af7181f7001da4586db74664e1d077
nihilne <85189523+nihilne@users.noreply.github.com>
2026-05-24 01:46:22 +0300

    refactor: a bunch of things

--- src/file36/cli.py
+++ src/file36/cli.py
@@ -1,6 +1,7 @@
 import sys
 import argparse
 import logging
+import threading
 from argparse import Namespace
 
 from file36.config import DEFAULT_VOLUME, DEFAULT_SPEED
@@ -21,18 +22,6 @@ handler.setFormatter(formatter)
 root.addHandler(handler)
 
 
-def mode_test(options: Namespace):
-    sender = Sender(Mode.TEXT, "TEST_DATA 123!@", options.speed, options.volume)
-    sender.play()
-
-    if options.save:
-        sender.export_wav("./output.wav")
-
-
-def mode_unimplemented(options: Namespace):
-    raise RuntimeError("Feature not implemented yet. Please use -t or --test.")
-
-
 def main():
     parser = argparse.ArgumentParser(
         prog="file36",
@@ -40,74 +29,78 @@ def main():
         epilog="By NIHILNE",
     )
 
-    mode = parser.add_mutually_exclusive_group(required=True)
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
-    mode.add_argument(
-        "-t",
-        "--text-mode",
-        action="store_const",
-        const=mode_unimplemented,
-        dest="func",
-        help="Encodes and plays the specified text.",
+    # PLAY PARSER & OPTIONS
+    play_parser = subparsers.add_parser("play", help="Encode and play audio.")
+
+    play_mode = play_parser.add_mutually_exclusive_group(required=True)
+    play_mode.add_argument(
+        "-t", "--text", metavar="TEXT", help="Encode and play a text string."
     )
-
-    mode.add_argument(
-        "-p",
-        "--path-mode",
-        action="store_const",
-        const=mode_unimplemented,
-        dest="func",
-        help="Encodes and plays a file from the specified path.",
+    play_mode.add_argument(
+        "-f", "--file", metavar="PATH", help="Encode and play a file."
     )
-
-    mode.add_argument(
-        "-b",
-        "--byte-mode",
-        action="store_const",
-        const=mode_unimplemented,
-        dest="func",
-        help="Encodes input to audio and plays it.",
+    play_mode.add_argument(
+        "-b", "--bytes", metavar="HEX", help="Encode and play raw bytes (hex)."
     )
-
-    mode.add_argument(
-        "-r",
-        "--receive",
-        action="store_const",
-        const=mode_unimplemented,
-        dest="func",
-        help="Listens for encoded audio and shows results.",
+    play_parser.add_argument(
+        "--save", action="store_true", help="Export the audio as a WAV file."
     )
-
-    mode.add_argument(
-        "--test",
-        action="store_const",
-        const=mode_test,
-        dest="func",
-        help="Plays test audio.",
+    play_parser.add_argument(
+        "--visualize", action="store_true", help="Show a spectrogram."
     )
-
-    parser.add_argument(
-        "--save",
-        action="store_true",
-        help="Saves either the played audio or the received audio.",
-    )
-
-    parser.add_argument(
+    play_parser.add_argument(
         "-v",
         "--volume",
         type=volume_type,
         default=DEFAULT_VOLUME,
-        help="Sets the volume. Accepts values from 1 to 100.",
     )
-
-    parser.add_argument(
+    play_parser.add_argument(
         "--speed",
         type=speed_type,
         default=DEFAULT_SPEED,
-        choices=list(Speed),
-        help="Sets the speed (or BPS) of the encoded audio.",
+        choices=[s.name for s in Speed],
+    )
+
+    # RECEIVE PARSER & OPTIONS
+    receive_parser = subparsers.add_parser(
+        "receive", help="Listen for and decode audio."
+    )
+
+    receive_parser.add_argument(
+        "--save", action="store_true", help="Export the audio as a WAV file."
     )
 
     args = parser.parse_args()
 
-    args.func(args)
+    if args.command == "play":
+        mode_play(args)
+    elif args.command == "receive":
+        mode_receive(args)
+
+
+def mode_play(options: Namespace):
+    if options.text:
+        sender = Sender(Mode.TEXT, options.text, options.speed, options.volume)
+    elif options.file:
+        sender = Sender(Mode.FILE, options.file, options.speed, options.volume)
+    elif options.bytes:
+        sender = Sender(
+            Mode.RAW, bytes.fromhex(options.bytes), options.speed, options.volume
+        )
+
+    if options.visualize:
+        play_thread = threading.Thread(target=sender.play)
+        play_thread.start()
+        sender.visualize()
+        play_thread.join()
+    else:
+        sender.play()
+
+    if options.save:
+        sender.export_wav("./output.wav")
+
+
+def mode_receive(options: Namespace):
+    raise RuntimeError("Not implemented yet.")
